7fda955e1841c789e62d0684596c73b7bd9e6c00
Amar Potki <a.potki@efarda.ir>
2022-04-21 18:24:47 +0430

    ordering

---
+++ src/Services/Ordering/Ordering.API/ProgramExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+
+namespace Ordering.API
+{
+    public static class ProgramExtensions
+    {
+        public static void ApplyDatabaseMigration<TContext>(this WebApplication app, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            // Apply database migration automatically. Note that this approach is not
+            // recommended for production scenarios. Consider generating SQL scripts from
+            // migrations instead.
+            using var scope = app.Services.CreateScope();
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+
+                    var retry = Policy.Handle<SqlException>()
+                        .WaitAndRetry(
+                            retryCount: 5,
+                            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // 2,4,8,16,32 sc
+                            onRetry: (exception, retryCount, context) =>
+                            {
+                                logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                            });
+
+                    //if the sql server container is not created on run docker compose this
+                    //migration can't fail for network related exception. The retry options for DbContext only 
+                    //apply to transient exceptions                    
+                    retry.Execute(() => InvokeSeeder(seeder, context, services));
+
+                    logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+                }
+                catch (SqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+                }
+            }
+
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Mapings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Commands.Orders.CheckoutOrder;
+using Ordering.Application.Features.Commands.Orders.UpdateOrder;
+using Ordering.Application.Features.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mapings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
